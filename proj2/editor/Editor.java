13ca1f92b00401131e9d5392a398bb9ce410f2fe
Ryan Church <Ryan Church>
2016-02-28 18:01:49 -0800

    Working draft of Editor.java

--- proj2/editor/Editor.java
+++ proj2/editor/Editor.java
@@ -1,3 +1,4 @@
+package editor;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.geometry.VPos;
@@ -10,139 +11,184 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.util.LinkedList;
-import Grid.java;
-
-/**
- * A JavaFX application that displays the letter the user has typed most recently in the center of
- * the window. Pressing the up and down arrows causes the font size to increase and decrease,
- * respectively.
- */
-
 
 public class Editor extends Application {
-    private static final int WINDOW_WIDTH = 500;
-    private static final int WINDOW_HEIGHT = 500;
+    private static int window_width = 500;
+    private static int window_height = 500;
+    private static int font_size = 12;
+    private static String font_name = "Verdana";
+    Grid parent = new Grid(font_size, font_name, window_width, window_height);
 
-    /** An EventHandler to handle keys that get pressed. */
     private class KeyEventHandler implements EventHandler<KeyEvent> {
-        int textCenterX;
-        int textCenterY;
+	private int fontSize = font_size;
+	private String fontName = "Verdana";
+	private int pointerXPos;
+	private int pointerYPos;
+	private Group root;
 
-        private static final int STARTING_FONT_SIZE = 20;
-        private static final double STARTING_TEXT_POSITION_X = 0;
-        private static final double STARTING_TEXT_POSITION_Y = 0;
-	Grid parent = new Grid(STARTING_FONT_SIZE, WINDOW_WIDTH, WINDOW_HEIGHT);
+	KeyEventHandler(final Group root, int x, int y) {
+	    pointerXPos = x;
+	    pointerYPos = y;
+	    this.root = root;
+	}
 
-        /** The Text to display on the screen. */
-        private Text currentText = new Text(STARTING_TEXT_POSITION_X, STARTING_TEXT_POSITION_Y, "");
-        private int fontSize = STARTING_FONT_SIZE;
-
-        private String fontName = "Verdana";
-
-
-        KeyEventHandler(final Group root, int windowWidth, int windowHeight) {
-	    textCenterX = fontSize/2;
-	    textCenterY = fontSize/2;
-
-            // Initialize some empty text and add it to root so that it will be displayed.
-            displayText = new Text(textCenterX, textCenterY, "");
-            // Always set the text origin to be VPos.TOP! Setting the origin to be VPos.TOP means
-            // that when the text is assigned a y-position, that position corresponds to the
-            // highest position across all letters (for example, the top of a letter like "I", as
-            // opposed to the top of a letter like "e"), which makes calculating positions much
-            // simpler!
-            displayText.setTextOrigin(VPos.TOP);
-            displayText.setFont(Font.font(fontName, fontSize));
-
-            // All new Nodes need to be added to the root in order to be displayed.
-            root.getChildren().add(displayText);
-        }
-
-        @Override
-        public void handle(KeyEvent keyEvent) {
-            if (keyEvent.getEventType() == KeyEvent.KEY_TYPED) {
-                // Use the KEY_TYPED event rather than KEY_PRESSED for letter keys, because with
-                // the KEY_TYPED event, javafx handles the "Shift" key and associated
-                // capitalization.
-                String characterTyped = keyEvent.getCharacter();
-                if (characterTyped.length() > 0 && characterTyped.chatAt(0) == 8) {
-                    // Ignore control keys, which have non-zero length, as well as the backspace
-                    // key, which is represented as a character of value = 8 on Windows.
-		    // CHANGE TO BE MADE: we need (List + characterTyped) rather than just characterTyped.
-                    displayText.setText(characterTyped);
-                    keyEvent.consume();
-                }
-
-                centerText();
-            }
-	    else if (keyEvent.getEventType() == KeyEvent.KEY_PRESSED) {
-                // Arrow keys should be processed using the KEY_PRESSED event, because KEY_PRESSED
-                // events have a code that we can check (KEY_TYPED events don't have an associated
-                // KeyCode).
-                KeyCode code = keyEvent.getCode();
-                if (code == KeyCode.UP) {
-                    fontSize += 5;
-                    displayText.setFont(Font.font(fontName, fontSize));
-                    centerText();
-                } else if (code == KeyCode.DOWN) {
-                    fontSize = Math.max(0, fontSize - 5);
-                    displayText.setFont(Font.font(fontName, fontSize));
-                    centerText();
-                }
-            }
-        }
-
-        private void centerText() {
-            // Figure out the size of the current text.
-            double textHeight = displayText.getLayoutBounds().getHeight();
-            double textWidth = displayText.getLayoutBounds().getWidth();
-
-            // Calculate the position so that the text will be centered on the screen.
-            double textTop = textCenterY - textHeight / 2;
-            double textLeft = textCenterX - textWidth / 2;
-
-            // Re-position the text.
-            displayText.setX(textLeft);
-            displayText.setY(textTop);
-
-            // Make sure the text appears in front of any other objects you might add.
-            displayText.toFront();
-        }
+		@Override
+		public void handle(KeyEvent keyEvent) {
+			if (keyEvent.getEventType() == KeyEvent.KEY_TYPED) {
+				Text displayText;
+				String characterTyped = keyEvent.getCharacter();
+				if (characterTyped.length() > 0 && characterTyped.charAt(0) != 8) {
+					displayText = parent.Type(characterTyped);
+					displayText.setTextOrigin(VPos.TOP);
+					displayText.setFont(Font.font(parent.fontName(), parent.fontSize()));
+					displayText.toFront();
+					root.getChildren().add(displayText);
+				}
+				keyEvent.consume();
+			}
+		}
     }
 
     @Override
     public void start(Stage primaryStage) {
-        // Create a Node that will be the parent of all things displayed on the screen.
-        Group root = new Group();
-        // The Scene represents the window: its height and width will be the height and width
-        // of the window displayed.
-        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT, Color.WHITE);
-
-        // To get information about what keys the user is pressing, create an EventHandler.
-        // EventHandler subclasses must override the "handle" function, which will be called
-        // by javafx.
-        EventHandler<KeyEvent> keyEventHandler =
-                new KeyEventHandler(root, WINDOW_WIDTH, WINDOW_HEIGHT);
-
-        // Register the event handler to be called for all KEY_PRESSED and KEY_TYPED events.
-        scene.setOnKeyTyped(keyEventHandler);
-        scene.setOnKeyPressed(keyEventHandler);
-
-        primaryStage.setTitle("Editor");
-
-        // This is boilerplate, necessary to setup the window where things are displayed.
-        primaryStage.setScene(scene);
-        primaryStage.show();
+		Group root = new Group();
+		Scene scene = new Scene(root, window_width, window_height, Color.WHITE);
+		EventHandler<KeyEvent> keyEventHandler = new KeyEventHandler(root, parent.leftMargin(), parent.topMargin());
+		scene.setOnKeyTyped(keyEventHandler);
+		scene.setOnKeyPressed(keyEventHandler);
+		primaryStage.setTitle("Editor");
+		primaryStage.setScene(scene);
+		primaryStage.show();
     }
-
-    
-    /**
-    public class OpenFile() {
-	
-    }
-    */
 
     public static void main(String[] args) {
-        launch(args);
+		launch(args);
+    }
+
+    public static class Grid {
+	Pointer pointer;
+	Line[] lines;
+	String fontName;
+	int fontSize;
+	int lineCount;
+	int topMargin;
+	int bottomMargin;
+	int leftMargin;
+	int rightMargin;
+	public class Pointer {
+	    private int xPosition;
+	    private int yPosition;
+	    public Grid parent;
+
+	    public Pointer(int x, int y, Grid p) {
+		xPosition = x;
+		yPosition = y;
+		parent = p;
+	    }
+
+	    public int getX() {
+		return xPosition;
+	    }
+	    public int getY() {
+		return yPosition;
+	    }
+		public void setxPosition(int x) {
+			xPosition = x;
+		}
+		public void setyPosition(int y) {
+			yPosition = y;
+		}
+	}
+
+	public class Line {
+	    private int yPosition;
+	    private int yCoordinate;
+	    private int lastXCoord;
+	    private LinkedList<String> contents;
+	    public Grid parent;
+
+	    public Line(int y, Grid p) {
+            parent = p;
+            yPosition = y;
+            yCoordinate = yPosition * parent.fontSize() + parent.topMargin();
+            lastXCoord = parent.leftMargin();
+            contents = new LinkedList<String>();
+	    }
+
+	    public Text Type(int i, String characterTyped) {
+			double dCharacterWidth = new Text(0, 0, characterTyped).getLayoutBounds().getWidth();
+            int characterWidth = (int) Math.ceil(dCharacterWidth);
+            if (characterWidth + this.lastXCoord > parent.rightMargin()) {
+                return parent.newLType(characterTyped);
+            } else {
+                Text displayText = new Text(lastXCoord, yCoordinate, characterTyped);
+                contents.add(i, characterTyped);
+                lastXCoord = lastXCoord + characterWidth;
+                return displayText;
+            }
+	    }
+	}
+
+
+
+	public Grid(int fontSize, String fontName, int windowWidth, int Windowheight) {
+	    rightMargin = windowWidth - 5;
+	    leftMargin = 5;
+	    bottomMargin = 0;
+	    topMargin = 0;
+	    lineCount = 32;
+	    this.fontSize = fontSize;
+	    this.fontName = fontName;
+	    lines = new Line[lineCount];
+        for (int i = 0; i <= lines.length - 1; i++) {
+            lines[i] = new Line(i, this);
+        }
+	    pointer = new Pointer(0, 0, this);
+	}
+
+	public Text Type(String characterTyped) {
+		Text returnText = lines[pointer.getY()].Type(pointer.getX(), characterTyped);
+		pointer.setxPosition(pointer.getX() + 1);
+		return returnText;
+	}
+
+        public Text newLType(String characterTyped) {
+            if (pointer.getY() == lines.length - 1) {
+                resizeLines();
+            }
+            pointer.setxPosition(0);
+            pointer.setyPosition(pointer.getY() + 1);
+            Text returnText = lines[pointer.getY()].Type(pointer.getX(), characterTyped);
+            pointer.setxPosition(pointer.getX() + 1);
+            return returnText;
+        }
+
+	public void resizeLines() {
+	    Line[] oldLines = lines();
+	    Line[] newLines = new Line[oldLines.length * 2];
+		System.arraycopy(oldLines, 0, newLines, 0, oldLines.length);
+	}
+
+	public int rightMargin() {
+	    return rightMargin;
+	}
+	public int leftMargin() {
+	    return leftMargin;
+	}
+	public int bottomMargin() {
+	    return bottomMargin;
+	}
+	public int topMargin() {
+	    return topMargin;
+	}
+	public String fontName() {
+	    return fontName;
+	}
+	public int fontSize() {
+	    return fontSize;
+	}
+	public Line[] lines() {
+	    return lines;
+	}
     }
 }
